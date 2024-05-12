9a686f9b992171c9cab84c123c36f8a66f2844de
Samir ABBOUD <es_abboud@esi.dz>
2024-05-12 23:40:59 +0200

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import {
+  createRouter,
+  createWebHistory
+} from 'vue-router'
+import AboutView from '@/views/AboutView.vue'
+import EventListView from '@/views/EventListView.vue'
+
+const router = createRouter({
+  history: createWebHistory(
+    import.meta.env.BASE_URL),
+  routes: [{
+      path: '/',
+      name: 'event-list',
+      component: EventListView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    }
+  ]
+})
+
+export default router
